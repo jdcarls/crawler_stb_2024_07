428598547fd5985e6cd79c03e62411ef7c298efe
JDC <jdcpr97@gmail.com>
2024-07-11 14:00:15 +0200

    Implementing filtering options, timestamp, unique-id, runtime in filter_data function and implement code for output to csv

--- crawler_stb_2024_07.py
+++ crawler_stb_2024_07.py
@@ -1,6 +1,10 @@
 from bs4 import BeautifulSoup as Bs
 import requests
 import pandas as pd
+import re
+import datetime
+import uuid
+import time
 
 # FUNCTION TO PRINT FIRST 30 ENTRIES INCLUDING NUMBER/ORDER, TITLE, POINTS AND COMMENTS
 
@@ -33,7 +37,7 @@ def fetch_data():
             score_subline = subtext.select_one('.score')
             # we are extracting only text using get_text() on the value stored in score_subline
             score_text = score_subline.get_text() if score_subline else '0 points'
-            # we are looking for the href tag and in case id has changed it will nevertheless be identified correctly
+            # we are looking for the href tag and in case it has changed, it will nevertheless be identified correctly
             matching_link = subtext.select('a[href*="item?id="]')
             comments_subline = matching_link[0] if matching_link else None
             comments_text = comments_subline.get_text().strip() \
@@ -42,7 +46,8 @@ def fetch_data():
             score_text = '0 points'
             comments_text = '0 comments'
 
-        # Creating dictionary 'result' for order, title, points, comments. Then appending results list
+        # Creating dictionary key-value pairs for order, title, points,comments that are saved in the dictionary result.
+        # Then appending results list
         result = {
 
             'order': i+1,
@@ -54,3 +59,73 @@ def fetch_data():
         results.append(result)
 
     return results
+
+
+# CREATING DATA FRAME FOR FILTERED DATA (The filter data function takes in the data from the fetch_data function)
+
+def filter_data(data):
+    # Storing start time for runtime calculation
+    start_time = time.time()
+
+    # Implementation of a unique identifier when running the function
+    identifier = str(uuid.uuid4())
+
+    # Now we are converting the returned list of dictionaries from the fetch data function into a data frame.
+    df = pd.DataFrame(data)
+    # Above we are using the Pandas library to create a DataFrame
+
+    # We need to extract the number of points and comments as number and comments also contain text
+    # Here we extract the numeric part of the string, which can be one or more digits - that's why we are using regex d+
+    # astype(int) then turns the extracted numerical string information into an integer
+    df['points'] = df['points'].str.extract('(\d+)').astype(int)
+    df['comments'] = df['comments'].str.extract('(\d+)').astype(int)
+
+    # Set up a new column in the data frame for word count as number of words is part of the filtering criteria
+    # We are applying the lambda function to the entries in the title column. The regular expression allows word count
+    # without punctuation
+    df['title_count_words'] = df['title'].apply(lambda x: len(re.findall(r'\b\w+\b', x)))
+
+    # FILTERING 1: MORE THAN 5 WORDS IN TITLE SORTED BY COMMENTS
+    filtered_by_comments_df = df[df['title_count_words'] > 5].sort_values(by='comments', ascending=False)
+
+    # FILTERING 2: LESS THAN OR EQUAL TO 5 WORDS IN TITLE SORTED BY POINTS
+    filtered_by_points_df = df[df['title_count_words'] <= 5].sort_values(by='points', ascending=False)
+
+    # Implementation of timestamp and filtering rule for each df
+    timestamp = datetime.datetime.now()
+    filtered_by_comments_df['timestamp'] = timestamp
+    filtered_by_comments_df['filter'] = "More than 5 words in title sorted by comments"
+
+    timestamp = datetime.datetime.now()
+    filtered_by_points_df['timestamp'] = timestamp
+    filtered_by_points_df['filter'] = "Less than or equal to 5 words in title sorted by points"
+
+    # Implementation of identifier for each df
+    filtered_by_comments_df['crawl_id'] = identifier
+    filtered_by_points_df['crawl_id'] = identifier
+
+    # Displaying the crawler version
+    crawler_version = "V. 1.0"
+    filtered_by_comments_df['crawler_version'] = crawler_version
+    filtered_by_points_df['crawler_version'] = crawler_version
+
+    # Storing end time of crawl for runtime calculation
+    end_time = time.time()
+
+    # Calculation of runtime
+    runtime = end_time - start_time
+
+    # Including the runtime in each df
+    filtered_by_comments_df['runtime'] = runtime
+    filtered_by_points_df['runtime'] = runtime
+
+    # Saving df in to a csv file
+    filtered_by_comments_df('filtered_by_comments_df.csv', index=False)
+    filtered_by_points_df('filtered_by_points_df.csv', index=False)
+
+    # Returning the filtered df
+    return filtered_by_comments_df, filtered_by_points_df
+
+
+data = fetch_data()
+filtered_data = filter_data(data)
